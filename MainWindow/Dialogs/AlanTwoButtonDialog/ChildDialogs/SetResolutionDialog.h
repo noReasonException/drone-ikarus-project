c554368e85d9e2d594a95f912437229078b49cf7
Stefanos Stefanou <stefannstefanou@gmail.com>
2018-04-11 17:46:38 +0300

    SetResolutionDialog State Subsystem implemented!

--- MainWindow/Dialogs/AlanTwoButtonDialog/ChildDialogs/SetResolutionDialog.h
+++ MainWindow/Dialogs/AlanTwoButtonDialog/ChildDialogs/SetResolutionDialog.h
@@ -10,23 +10,48 @@
 #include "../AlanTwoButtonDialog.h"
 #include "../../../../misc/img/generic_paths.h"
 #include "../../../State/WindowStates/AlanTwoButtonsDialogState/AlanTwoButtonsDialogState.h"
+#include "../../../State/WindowStates/AlanTwoButtonsDialogState/ChildStates/SetResolutionDialogState.h"
 
 class SetResolutionDialog : public AlanTwoButtonDialog {
 public:
     SetResolutionDialog(AlanTwoButtonsDialogState *state):AlanTwoButtonDialog(state,RESOLUTION_DIALOG_TITLE,RESOLUTION_ICON){}
 
 private:
+    QLineEdit *width;
+    QLineEdit *height;
+
     QWidget *onGenerateConfigArea() throw (std::exception)override {
         QWidget*widget=new QWidget;
         QGridLayout*lay;
         widget->setLayout(lay=new QGridLayout);
         lay->addWidget(new QLabel("Width:"),0,0);
         lay->addWidget(new QLabel("Height:"),1,0);
-        lay->addWidget(new QLineEdit,0,1);
-        lay->addWidget(new QLineEdit,1,1);
+        lay->addWidget(width=new QLineEdit,0,1);
+        lay->addWidget(height=new QLineEdit,1,1);
         return widget;
 
 
+    }
+
+protected:
+    SetResolutionDialogState *onRestoreState() throw(std::exception)override {
+            SetResolutionDialogState*thisState= dynamic_cast<SetResolutionDialogState*>
+                                (AlanTwoButtonDialog::onRestoreState());
+            width->setText(thisState->width);
+            height->setText(thisState->height);
+            return thisState;
+    }
+    SetResolutionDialogState *onSaveState() throw(std::exception)override {
+            SetResolutionDialogState*thisState= dynamic_cast<SetResolutionDialogState*>
+                                (AlanTwoButtonDialog::onRestoreState());
+            thisState->width=width->text();
+            thisState->height=height->text();
+            return thisState;
+    }
+
+    bool generic_initializer() override {
+            return AlanTwoButtonDialog::generic_initializer()&&
+                   onRestoreState();
     }
 
 };
--- MainWindow/State/WindowStates/AlanTwoButtonsDialogState/ChildStates/SetResolutionDialogState.h
+++ MainWindow/State/WindowStates/AlanTwoButtonsDialogState/ChildStates/SetResolutionDialogState.h
@@ -11,11 +11,23 @@
 
 class SetResolutionDialogState: public AlanTwoButtonsDialogState {
 public:
-    SetResolutionDialogState() = default;
-
+    SetResolutionDialogState(){
+        if(State::settings.contains(createPath()+RESOLUTION_WIDTH_QLINEEDIT_STATE) &&
+                State::settings.contains(createPath()+RESOLUTION_HEIGHT_QLINEEDIT_STATE)){
+            width=State::settings.value(createPath()+RESOLUTION_WIDTH_QLINEEDIT_STATE).toString();
+            height=State::settings.value(createPath()+RESOLUTION_HEIGHT_QLINEEDIT_STATE).toString();
+            return ;
+        }
+        width="0";
+        height="0";
+    };
+    QString width;
+    QString height;
     virtual ~SetResolutionDialogState() = default;
 
     void update() override {
+        settings.setValue(createPath()+RESOLUTION_WIDTH_QLINEEDIT_STATE,width);
+        settings.setValue(createPath()+RESOLUTION_HEIGHT_QLINEEDIT_STATE,height);
     }
 
     QString createPath() override {
